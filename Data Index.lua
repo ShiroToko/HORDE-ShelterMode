3920507e2ae4b17bad78be7305d00534e197b05f
ShiroToko <140574162+ShiroToko@users.noreply.github.com>
2023-07-26 22:13:17 +0800

    Read this for Index for data table

---
+++ Data Index.lua
@@ -0,0 +1,57 @@
+						--[[ 
+
+              These are index of PlayerDataTable, With this you can understand more easier what going on in ZShelterSV.lua
+
+							idx[01] = Steam ID -> ID that used to auth user
+							idx[02] = Wood -> def 0
+							idx[03] = Iron -> def 0
+							idx[04] = Wood Limit -> def 12
+							idx[05] = Iron Limit -> def 10
+							idx[06] = Limit Scaling -> def 1
+							idx[07] = Resource Scaling -> def 1
+							idx[08] = Remote Storaging -> def 0
+							idx[09] = Fixspeed -> def 1
+							idx[10] = Upgrade material -> def 0
+							idx[11] = Auto fix -> def 0
+							idx[12] = Auto heal -> def 0
+							idx[13] = Building HP scaling -> def 0
+							idx[14] = HP boost -> def 0
+							idx[15] = Turret Damage Scaling - > def 1
+							idx[16] = Self Heal -> def 0
+							idx[17] = ent -> Player
+							idx[18] = Pocket Storage -> def 0
+							idx[19] = Player Sanity Drop Interval -> def 1
+
+							----- [Skill Tree] -----
+							idx[20] = Skill Points -> def 1
+
+							-> Skill Sync Data
+								idx[21] = HP Boost
+								idx[16] = Self Heal
+								idx[22] = Life Stealing
+								idx[23] = Armor Boost
+								idx[24] = Armor On KIill
+
+								idx[25] = Picking
+								idx[26] = Capacity
+								idx[08] = Remote Storaging
+								idx[19] = Player Sanity Drop Interval
+								idx[27] = Build Speed
+								idx[28] = Build HP
+								idx[29] = Turret Damage
+								idx[30] = Building Level
+								idx[31] = PocketStorage
+								idx[32] = GunLevel
+								idx[11] = Auto Fix
+								idx[34] = Campfire
+								idx[35] = Resource Rader
+								idx[36] = Looting
+								idx[37] = Resource saving
+								idx[38] = Airstrike [Ultimate]
+								idx[39] = Cloak [Ultimate]
+							----- [Skill Tree] -----
+
+							idx[33] = Rescue Sent - > def false
+							idx[40] = cloak state -> def false
+
+						]]
